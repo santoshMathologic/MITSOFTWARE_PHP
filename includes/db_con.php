576643c152a14b1aeac54b60f59879b8dfd6ef67
santosh <santosh_citech@live.in>
2017-10-13 12:17:15 +0530

    login page changes

--- includes/db_con.php
+++ includes/db_con.php
@@ -1,14 +1,15 @@
 <?php
 error_reporting(E_ERROR | E_PARSE);
+//phpinfo();
 session_start();
 		$dbhost = 'localhost:3306';
 		$dbuser = 'root';
 		$dbpass = 'root';
 		$dbname = 'mitsoftware';
-     	$conn=mysqli_connect($dbhost,$dbuser,$dbpass);
-
-mysqli_select_db($con,$dbname); 
-session_start();
+     	$conn	= mysqli_connect($dbhost,$dbuser,$dbpass);
+		
+		 mysqli_select_db($con,$dbname); 
+         session_start();
 		if(! $conn ) {
 		   die('Could not connect: ' . mysql_error());
 		}
--- index.php
+++ index.php
@@ -1,169 +1,61 @@
-<?php
-include("includes/db_con.php");
-$errors = "";
-if(isset($_POST['submit']))
-{
-	$user_mail = $_POST['user_mail'];
-	$password = $_POST['password'];
-	$query="SELECT *FROM user_login where user_mail='$user_mail' AND password='$password'";
-  	$result=mysqli_query($query);
-  	mysqli_error();
-  	if(mysqli_num_rows($result)>0)
-  	{
-		$row = mysqli_fetch_array($result);
-		$_SESSION["AdminId"] = $row["user_mail"];
-		$_SESSION['user_type_fk'] = $row['user_type_fk'];
-		
-		if($_SESSION['user_type_fk']==1)
-        {
-        header("location:home.php");
-        }
-		if($_SESSION['user_type_fk']==2)
-		{
-		header("location:index.php");
-		}
-		if($_SESSION['user_type_fk']==3)
-		{
-		header("location:index.php");
-		}
-		
-		
-		die();
-	}
-  	else
-  	{	$errors = "Invalid username/password";
-		
-	}
-}
-if(isset($_POST['cancel']))
-{
-	header("location:home.php");
-}
-?>
+
+
 <!DOCTYPE html>
 <html>
 <head>
-
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<link rel="stylesheet" href="css/style.css">
-<link rel="stylesheet" type="text/css" href="css/styleadmin.css"/>
-<link rel="shortcut icon" type="image/x-icon" href="images/icon/favicon.ico" />
-
-
-
-<title>Admin Login</title>
-<style>
-.error
-{
-	color:#F00;
-	font-size:18px;
-	margin-left:50px;
-}
-</style>
-
-
-<script>
-function var_admin()
-{
-var user_mail = document.getElementById('user_mail').value;
-		
-		if(user_mail == '') {
-			alert('Enter the Username');
-			document.getElementById('user_mail').focus();
-			return false;
-		}
-		var password = document.getElementById('password').value;
-		
-		if(password == '') {
-			alert('Enter the Password');
-			document.getElementById('password').focus();
-			return false;
-		}
-}
-</script>
-<style>
-.background
-{
-/*background-image:url(images/Back.png); */
-/*background-repeat:no-repeat; */
-
-background-color:#313131;
-
-}
-</style>
-
-
-
-
+<title>Page Title</title>
 </head>
+<body>
+<div class="container">
+<div class="row">
+    <div class="col-sm-6 col-md-4 col-md-offset-4">
 
-<body id="Gradiant_body" class="background">
- 
-  <form name="form_Login" id="form_login" action="" onSubmit="return var_admin()" method="post">
-<div id="page">
-<div style="width:30px;float:left;margin-left:10px;">
-          
-          
-           <div style="margin-top:0px; margin-left:670px;">
-<embed src=http://flash-clocks.com/free-flash-clocks-blog-topics/free-flash-clock-121.swf width=200 height=100 wmode=transparent type=application/x-shockwave-flash></embed>
-        </div>
-          
-          
-          
-          </div>
-          
-
-<div id="mid" style="margin:0px;">
-  <div id="log">
-  	<div style="margin-left:30px;width:349px;"><img src="images/Main_logo.png"/></div>
-
- 
-  <h3>Admin Login</h3>
-  </div>
-   <?php
-          
-		  if($errors != "")
-		   {echo '<p class="error">'. $errors .'</p>';
-		   }
-		  
-		  ?> 
-          
-          
-       
-          
-           
-         <div style="width:150px;float:left;margin-left:40px;">
-           <a href="index.php" style="font-family:'Times New Roman', Times, serif; text-decoration:none;color:#999">Admin</a> <br/><br/>
-          <a href="employeeapp.php" style="text-decoration:none;font-family:'Times New Roman', Times, serif;color:#999">Employee</a><br/><br/>
-          <a href="timeapp.php" style="text-decoration:none;font-family:'Times New Roman', Times, serif;color:#999">Timesheet Approval</a> 
-          </div>
-         
+    <div style="margin-left:30px;width:349px;"><img src="images/Main_logo.png"></div>
     
-      <div style="width:300px;float:right;;margin-right:100px;margin-top:20px;">          
- 
-    <div id="txtdiv">
-       <input type="text" name="user_mail" id="user_mail" class="txt" autocomplete="off" placeholder="UserName" class="txtBox">  
+        
+        
+        <div class="panel panel-default">
+      <div class="panel-heading">Sign In</div>
+      <div class="panel-body">
+      <form class="form-signin">
+            <input type="text" class="form-control" placeholder="Email" required autofocus>
+            <input type="password" class="form-control" placeholder="Password" required>
+            <button class="btn btn-lg btn-primary btn-block" type="submit">
+                Sign in</button>
+            <label class="checkbox pull-left" style="margin-left: 20px;">
+                <input type="checkbox" value="remember-me">
+                Remember me
+            </label>
+            <a href="#" class="pull-right need-help">Need help? </a><span class="clearfix"></span>
+            </form>
+      
+      
+      
+      
+      </div>
     </div>
- 
- 
-    <div id="txtdiv">
-       <input type="password" name="password" id="password" class="txtpass" autocomplete=off placeholder="Password">  
+           
+
+
+          
+        
+        <a href="#" class="text-center new-account">Create an account </a>
     </div>
-
-
-<button type="submit" value="Login" name="submit" style="width:118px;height:35px;margin-top:17px; margin-right:0px; margin-left:172px">
-<img src="images/Button_Gradiant/00080.png" name="submit" style="margin-top:-3px; margin-left:-5px; border-radius:3px; border:0px solid  rgb(0,0,0)"/>
-</button> 
-    <div style="clear:both"></div>
-
-
-
-    <div style="clear:both"></div>
-
-   
 </div>
 </div>
-</div>
-</form>
+
+<div style="margin-top: -226px;">
+           <a href="index.php" >Admin</a> <br/><br/>
+          <a href="employeeapp.php">Employee</a><br/><br/>
+          <a href="timeapp.php">Timesheet Approval</a> 
+          </div>
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
 </body>
 </html>
+
+
+
